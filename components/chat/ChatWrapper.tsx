51cfc477e4c37121fca96cf83d6cc017761b422f
Rachel Harris <fauxenouement@gmail.com>
2026-05-20 13:57:37 -0500

    Add ChatWrapper component for chat functionality

---
+++ components/chat/ChatWrapper.tsx
@@ -0,0 +1,41 @@
+import { defineQuery } from "next-sanity";
+import Chat from "@/components/chat/Chat";
+import { sanityFetch } from "@/sanity/lib/live";
+import type { CHAT_PROFILE_QUERY_RESULT } from "@/sanity.types";
+import SidebarToggle from "../SidebarToggle";
+
+const CHAT_PROFILE_QUERY = defineQuery(`*[_id == "singleton-profile"][0]{
+    _id,
+    _type,
+    _createdAt,
+    _updatedAt,
+    _rev,
+    firstName,
+    lastName,
+    headline,
+    shortBio,
+    email,
+    phone,
+    location,
+    availability,
+    socialLinks,
+    yearsOfExperience,
+    profileImage
+  }`);
+
+async function ChatWrapper() {
+  const { data } = await sanityFetch({ query: CHAT_PROFILE_QUERY });
+  const profile = data as CHAT_PROFILE_QUERY_RESULT;
+
+  return (
+    <div className="h-full w-full">
+      <div className="md:hidden p-2 sticky top-0 z-10">
+        <SidebarToggle />
+      </div>
+
+      <Chat profile={profile} />
+    </div>
+  );
+}
+
+export default ChatWrapper;
