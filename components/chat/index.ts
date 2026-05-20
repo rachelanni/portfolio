013d9df523db9f0be73ce748432275be4a8aa604
Rachel Harris <fauxenouement@gmail.com>
2026-05-20 13:57:02 -0500

    Add ChatKit exports in index.ts

---
+++ components/chat/index.ts
@@ -0,0 +1,8 @@
+export type * from '@openai/chatkit-react';
+export { ChatKit } from '@openai/chatkit-react';
+export {
+    useChatKit,
+    type UseChatKitOptions,
+    type UseChatKitReturn,
+    type ChatKitControl,
+} from '@/components/hooks/useChatkit';
