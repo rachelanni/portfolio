4a79bd991484ae1801bdf9cc840fc465b2f79bec
Rachel Harris <fauxenouement@gmail.com>
2026-05-20 14:35:24 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
+  allowedDevOrigins: [
+    "http://localhost:3001/*",
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://187.124.68.21:*/*",
+    "http://187.124.68.21:3000/studio/*",
+    "http://187.124.68.21:3001/studio/*",
+    "https://studio.jiriki.tech*",
+  ],
   /* config options here */
   images: {
     remotePatterns: [
