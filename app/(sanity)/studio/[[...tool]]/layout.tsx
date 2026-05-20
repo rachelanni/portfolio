4a7ae332d7f69e6bc23199134529765053eccb6f
Rachel Harris <fauxenouement@gmail.com>
2026-05-20 13:47:55 -0500

    Add StudioLayout component for Next.js Sanity studio

---
+++ app/(sanity)/studio/[[...tool]]/layout.tsx
@@ -0,0 +1,11 @@
+import { NextStudioLayout } from 'next-sanity/studio';
+
+export default function StudioLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0 }}>
+        <NextStudioLayout>{children}</NextStudioLayout>
+      </body>
+    </html>
+  );
+}
