f5a29dba8511b8034de57ea924adb87bdcd66942
Rachel Harris <fauxenouement@gmail.com>
2026-05-20 14:20:45 -0500

    Create live.ts

---
+++ sanity/lib/live.ts
@@ -0,0 +1,12 @@
+// Querying with "sanityFetch" will keep content automatically updated
+// Before using it, import and render "<SanityLive />" in your layout, see
+// https://github.com/sanity-io/next-sanity#live-content-api for more information.
+
+import { defineLive } from 'next-sanity/live';
+import { client } from './client';
+
+export const { sanityFetch, SanityLive } = defineLive({
+  client: client.withConfig({ apiVersion: '2026-05-11' }),
+  serverToken: process.env.SANITY_API_READ_TOKEN,
+  browserToken: process.env.SANITY_API_READ_TOKEN,
+});
